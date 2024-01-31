6dec7c072ee16dcc60d6a8b5f68cdecc931ac67e
Jane Xu <janeyx@meta.com>
2024-01-31 13:49:14 -0800

    [optim] Rectify capturable testing and fix bugs!
    ghstack-source-id: a7ba3ccad5871eb733261f27103d05e636645aef
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118326

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -1568,7 +1568,8 @@ class TorchPatcher:
         }
 
         excluded_single_tensor = {
-            radam,  # https://github.com/pytorch/pytorch/issues/117807
+            radam,  # https://github.com/pytorch/pytorch/issues/118230
+            adamax,  # https://github.com/pytorch/pytorch/issues/117836
         }
 
         for opt_mod in optimizer_modules:
